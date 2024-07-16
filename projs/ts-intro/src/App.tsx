db9b51a654590532fa957a0273c495e038121c4f
musaahmedli <adnsu.musa@mail.ru>
2024-07-16 22:51:08 +0300

    firstcommit

---
+++ projs/ts-intro/src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+   interface Iperson {
+    [key:string]: string|number 
+}
+ const person:Iperson ={
+    name:"Anar",
+    surname:"Soltan",
+    age:25,
+}
+console.log( "-----",person.Ispossitive)
+console.log("object");
+
+  return (
+    <>
+      <h1>
+       salam
+       
+      </h1>
+    </>
+  );
+}
+
+export default App;
